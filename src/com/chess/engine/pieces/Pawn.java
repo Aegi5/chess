fe85cf416d7838c8b9e6f0756d853c8c9b0cb2f1
Aegis <hyunchulchoi98@gmail.com>
2020-11-28 20:14:21 +0100

    Finished pawn III (will come back to it later for attack move and 'en passant'

--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -4,14 +4,14 @@ import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
+import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class Pawn extends Piece{
-    private static final int[] CANDIDATE_MOVE_VECTOR_COORDINATES= {8, 16};
-    private boolean isFirstMove;
+    private static final int[] CANDIDATE_MOVE_VECTOR_COORDINATES= {8,16, 7, 9};
 
     Pawn(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
@@ -47,9 +47,34 @@ public class Pawn extends Piece{
 
 
             }
+            else if(currentCandidateOffset == 7 &&
+                    (BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.getPieceAlliance().isWhite())
+                || (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.getPieceAlliance().isBlack()) ){
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if(pieceOnCandidate.getPieceAlliance() != this.getPieceAlliance()){
+                        //TODO: attack move ?
+                        legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, pieceOnCandidate));
+                    }
+
+                }
+
+            }
+            else if(currentCandidateOffset == 9 &&
+                    !( (BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.getPieceAlliance().isBlack())
+                    || (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.getPieceAlliance().isWhite()) ) ) {
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if(pieceOnCandidate.getPieceAlliance() != this.getPieceAlliance()){
+                        //TODO: attack move ?
+                        legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, pieceOnCandidate));
+                    }
+
+                }
+            }
             //pawn promotion upon reaching first or last row
 
         }
-        return null;
+        return ImmutableList.copyOf(legalMoves);
     }
 }
